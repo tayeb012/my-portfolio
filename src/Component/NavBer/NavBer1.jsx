05248453965b898af6a93e2ac58a07444b00beb3
Tayeb Hossan <tayebhoss80@gmail.com>
2023-12-17 23:34:30 +0600

    fixed some error

---
+++ src/Component/NavBer/NavBer1.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const NavBer1 = () => {
+  const [clops, setClops] = useState(false);
+  const links = (
+    <>
+      <li className="hover:text-[#C99976]">
+        <a href="#about">About</a>
+      </li>
+      <li className="hover:text-[#C99976]">
+        <a href="#mySkill">My Skill</a>
+      </li>
+      <li className="hover:text-[#C99976]">
+        <a href="#project">Project</a>
+      </li>
+      <li className="hover:text-[#C99976]">
+        <a href="#contact">Contact</a>
+      </li>
+    </>
+  );
+  return (
+    <div className="flex justify-between xl:text-4xl md:text-2xl">
+      <h2 className="text-2xl md:text-3xl lg:text-5xl">Portfolio</h2>
+      <section className="">
+        <div className="md:hidden text-2xl" onClick={() => setClops(!clops)}>
+          <FaRegWindowClose
+            className={` transition-transform duration-1000 ${
+              !clops && "hidden"
+            }`}
+          />
+          <IoReorderThreeOutline
+            className={` transition-transform duration-1000 ${
+              clops && "hidden"
+            }`}
+          />
+        </div>
+        <ul className=" relative md:hidden text-right flex flex-col lg:gap-10 ">
+          <span
+            className={`glass p-2 absolute transform ${
+              clops ? "-translate-x-20" : "-translate-x-[40rem]"
+            } transition-transform duration-1000`}
+          >
+            {links}
+          </span>
+        </ul>
+        <ul className="hidden  md:flex  lg:gap-5 xl:gap-10 md:gap-2">
+          {links}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default NavBer1;
